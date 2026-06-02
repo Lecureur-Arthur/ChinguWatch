aca720712a3343e824ff3693c32283f4b565cccb
Arthur LECUREUR <arthur.lecureur@gmail.com>
2026-06-02 17:04:03 +0200

    feat: add Vite configuration for production deployment and GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuration de Vite adaptée pour le déploiement en production
 export default defineConfig({
+  // Je définis le chemin de base avec le nom exact de mon dépôt GitHub entre barres obliques
+  base: '/chinguwatch/',
   plugins: [react()],
 })
