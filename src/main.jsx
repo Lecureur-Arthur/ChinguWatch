ee8d16b8e06497b8aca05b205b55f73485f6f4ef
Arthur LECUREUR <arthur.lecureur@gmail.com>
2026-06-21 16:30:10 +0200

    Correct Bug F5

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// 1. Importe HashRouter au lieu de BrowserRouter
+import { HashRouter } from 'react-router-dom'
 import './App.css'
 import App from './App.jsx'
 
-// J'initialise l'application React et je l'encapsule dans le routeur principal avec le bon basename
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ChinguWatch">
+    {/* 2. Remplace BrowserRouter par HashRouter */}
+    {/* Note : le basename n'est généralement plus nécessaire avec HashRouter */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
